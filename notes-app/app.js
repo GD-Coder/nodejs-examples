7de786390f2249a7595836648fb8d784cad050de
Downey <GYMJN0@usa.dce.usps.gov>
2019-05-28 15:35:10 -0500

    Lesson 12- Challenge, with bonus points.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,19 @@
 const add = require('./utils')
 const getNotes = require('./notes')
 const validator = require('validator')
+const chalk = require('chalk')
 const sum = add(6, 9)
+const log = (data) => console.log(data)
 
 
-console.log(getNotes(1))
+log(getNotes(1))
 
-console.log(validator.isEmail('jimmy@jammy.com'))
+log(validator.isEmail('jimmy@jammy.com'))
+
+log(chalk.yellow.bold.inverse('Success!'))
+
+log(chalk.red.bold('Success!'))
+
+log(chalk.green('Success!'))
+
+log(chalk.blue(sum))

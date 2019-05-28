47b0cb4749f5920a6cef4ba3925b68cf19770746
Downey <GYMJN0@usa.dce.usps.gov>
2019-05-28 15:17:33 -0500

    Lesson 11- Importing NPM Modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,9 @@
 const add = require('./utils')
 const getNotes = require('./notes')
+const validator = require('validator')
 const sum = add(6, 9)
 
 
 console.log(getNotes(1))
+
+console.log(validator.isEmail('jimmy@jammy.com'))

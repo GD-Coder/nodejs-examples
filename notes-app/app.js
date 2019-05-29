f13cba4883c03f5c2f593e4cb34a0fee7cd58f7e
Downey <GYMJN0@usa.dce.usps.gov>
2019-05-29 08:51:52 -0500

    Lesson 17- Challenge

--- notes-app/app.js
+++ notes-app/app.js
@@ -7,12 +7,37 @@ const log = console.log
 yargs.command({
     command: "add",
     describe: "Add a new note",
-    handler: () => log("added new note")
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: "Note contents",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) =>
+        log(
+            chalk.yellow.bold("Title: ") +
+            chalk.blue(argv.title) + ", " +
+            chalk.yellow.bold("Body: ") +
+            chalk.blue(argv.body)
+        )
 })
 
 yargs.command({
     command: "remove",
     describe: "Remove a new note",
+    builder: {
+        title: {
+            describe: "Note title to remove",
+            demandOption: true,
+            type: 'string'
+        }
+    },
     handler: () => log("removing note")
 })
 
@@ -25,5 +50,14 @@ yargs.command({
 yargs.command({
     command: "read",
     describe: "Read an added note",
+    builder: {
+        title: {
+            describe: "Note title to read",
+            demandOption: true,
+            type: 'string'
+        }
+    },
     handler: () => log("reading a note")
 })
+
+yargs.parse()

c2d6e0800d16841bd62a136a26c78cf12382f79f
Downey <GYMJN0@usa.dce.usps.gov>
2019-05-30 08:57:34 -0500

    Lesson 32- Challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,20 @@
+const chalk = require("chalk")
+const request = require("request")
 const log = console.log
 
-log("Starting...")
+const displayColoredResult = (color, message, option) =>
+  option ? log(chalk[option][color](message)) : log(chalk[color](message))
 
+const url =
+  "https://api.darksky.net/forecast/c7ce5c066275d2ab3566eb37ea9fe713/35.4676,-97.5164"
 
-setTimeout(() => {
-    log("2 Second timer...")
-}, 2000)
-
-
-
-log("Stopping...")
+request({ url: url, json: true }, (error, response) => {
+  displayColoredResult(
+    "green",
+    "It is currently " +
+      JSON.stringify(response.body.currently.temperature) +
+      " degrees out. There is a " +
+      JSON.stringify(response.body.currently.precipProbability) +
+      "% chance of rain."
+  )
+})

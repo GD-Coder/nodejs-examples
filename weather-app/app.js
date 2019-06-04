e2c038214ed751a20d30ac99b3fcae621a08814f
Downey <GYMJN0@usa.dce.usps.gov>
2019-06-04 09:11:28 -0500

    Added error service to display human readable errors.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,20 +1,48 @@
 const chalk = require("chalk")
 const request = require("request")
+const geocodeService = require("./geocode")
+const errorService = require("./error")
 const log = console.log
 
 const displayColoredResult = (color, message, option) =>
-  option ? log(chalk[option][color](message)) : log(chalk[color](message))
+	option ? log(chalk[option][color](message)) : log(chalk[color](message))
 
-const url =
-  "https://api.darksky.net/forecast/c7ce5c066275d2ab3566eb37ea9fe713/35.4676,-97.5164"
+var weatherUrl
 
-request({ url: url, json: true }, (error, response) => {
-  displayColoredResult(
-    "green",
-    "It is currently " +
-      response.body.currently.temperature +
-      " degrees out. There is a " +
-      response.body.currently.precipProbability +
-      "% chance of rain."
-  )
+request({
+	url: geocodeService.returnGeocode(),
+	json: true
+}, (error, response) => {
+	if (error) {
+		displayColoredResult("red", errorService.returnErrorResponse("location", "connection"))
+	} else if (!response.body.features) {
+		displayColoredResult("red", errorService.returnErrorResponse("location", "location"))
+	} else {
+		weatherUrl = "https://api.darksky.net/forecast/c7ce5c066275d2ab3566eb37ea9fe713/" + response.body.features[0].center.reverse().toString()
+	}
 })
+
+console.log(geocodeService.returnGeocode())
+
+setTimeout(() => {
+	request({
+		url: weatherUrl,
+		json: true
+	}, (error, response) => {
+		if (error) {
+			displayColoredResult("red", errorService.returnErrorResponse("weather", "connection"))
+		} else if (response.body.error) {
+			displayColoredResult("red", errorService.returnErrorResponse("weather", "location"))
+		} else {
+			displayColoredResult(
+				"green",
+				"It is currently " +
+				response.body.currently.temperature +
+				" degrees out. There is a " +
+				response.body.currently.precipProbability +
+				"% chance of rain. " +
+				response.body.daily.data[0].summary
+			)
+		}
+	})
+}, 2000)

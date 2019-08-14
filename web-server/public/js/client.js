fda62f9f9e43cc3936dd11b96843aa4d947eb15a
Downey <GYMJN0@usa.dce.usps.gov>
2019-08-14 14:32:00 -0500

    Set up app for cloud deployment

--- web-server/public/js/client.js
+++ web-server/public/js/client.js
@@ -20,18 +20,16 @@ weatherForm.addEventListener("submit", event => {
   event.preventDefault()
   const loading = setInterval(appendEllipsis, 1000)
   loader.textContent = "Fetching forecast, please wait."
-  fetch(`http://localhost:3000/weather?address=${search.value}`).then(
-    response => {
-      response.json().then(data => {
-        clearInterval(loading)
-        loader.textContent = ""
-        if (data.error) {
-          error.textContent = data.error
-        } else {
-          city.textContent = data.location
-          forecast.textContent = data.forecast
-        }
-      })
-    }
-  )
+  fetch(`/weather?address=${search.value}`).then(response => {
+    response.json().then(data => {
+      clearInterval(loading)
+      loader.textContent = ""
+      if (data.error) {
+        error.textContent = data.error
+      } else {
+        city.textContent = data.location
+        forecast.textContent = data.forecast
+      }
+    })
+  })
 })
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,6 +6,7 @@ const geocode = require("./utils/geocode")
 const forecast = require("./utils/forecast")
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicPath = path.join(__dirname, "../public")
@@ -78,6 +79,6 @@ app.get("*", (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log("Server is started on port 3000...")
+app.listen(port, () => {
+  console.log(`Server is started on port ${port}...`)
 })
